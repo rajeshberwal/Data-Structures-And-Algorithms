0e90e2a9ab1dbb75344cdc5b631703997da34e35
Rajesh Kumar <irajeshberwal@gmail.com>
2022-05-26 09:28:41 +0530

    Contains Duplicate

--- Leetcode/Contains-Duplicate/contains-duplicate.py
+++ Leetcode/Contains-Duplicate/contains-duplicate.py
@@ -1,5 +1,11 @@
 from typing import List
 
 def containsDuplicate(nums: List[int]) -> bool:
-    n = len(nums)
-    return sum(nums) == (n * (n + 1)//2)
+    numMap = {}
+
+    for num in nums:
+        if num in numMap:
+            return True
+        numMap[num] = 1
+    
+    return False
